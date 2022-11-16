7dfcb63587f5b2f35b35a16fbf06c0469e8f3038
Timothy Jones Ong <tong6@my.centennialcollege.ca>
2022-11-16 14:53:04 -0500

    Uploaded Initial App

---
+++ Comp367_Lab3_TJO_WebApplication/Pages/Privacy.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Comp367_Lab3_TJO_WebApplication.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+
+        public PrivacyModel(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+# Comp367 Lab 3 - Azure DevOps Boards
+
+To understand Azure DevOps Boards
+To Become familiar with Basic and Agile Process
