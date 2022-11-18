f4cfc2137c02702567be0f86fceb02554858fc39
Timothy Jones Ong <tong6@my.centennialcollege.ca>
2022-11-17 21:25:46 -0500

    Added Validation

--- Comp367_Lab3_TJO_WebApplication/Migrations/20221118022331_New_DataAnnotations.Designer.cs
+++ Comp367_Lab3_TJO_WebApplication/Migrations/20221118022331_New_DataAnnotations.Designer.cs
@@ -4,6 +4,7 @@ using Comp367_Lab3_TJO_WebApplication.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Comp367Lab3TJOWebApplication.Migrations
 {
     [DbContext(typeof(MovieContext))]
-    partial class MovieContextModelSnapshot : ModelSnapshot
+    [Migration("20221118022331_New_DataAnnotations")]
+    partial class NewDataAnnotations
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
